6eb65faa707064a7e7e451d8caf707e40e2ade10
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-09 18:12:33 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,17 @@
+class C{
+public static void main(String []args){
+System.out.println("The two number is:");
+int a=10;
+int b=2;
+System.out.println(a +" "+b);
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+
+System.out.println("The sum is :" + sum);
+System.out.println("The div is :" + sub);
+System.out.println("The mul is :" + mul);
+System.out.println("The sub is :" + div);
+
+}}
