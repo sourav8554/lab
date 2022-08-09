eb3c6b74414f5a473db402eb84948ed41c2c539a
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-09 17:49:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition{
+public static void main(String []args){
+System.out.println("The two number is:");
+int a=10;
+int b=2;
+System.out.println(a +" "+b);
+int sum=a+b;
+System.out.println("The sum is :" + sum);
+
+}}
