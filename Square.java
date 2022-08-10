b9eb13b45af807ed73f73d4e802f10394ccbc40a
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-10 15:46:01 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,18 @@
+class Square{
+
+public static void main(String args[]){
+
+
+System.out.println("the side of square is  " +12);
+
+int s=12;
+
+int area_square=s*s;
+
+int peri=s*4;
+
+System.out.println("Area of the square="+area_square);
+
+System.out.println("perimeter of the square="+peri);
+
+}}
