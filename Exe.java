8a12c07f72cc6965a5d918e9296b86011ccb812c
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-17 14:20:37 +0530

    Add files via upload

---
+++ Exe.java
@@ -0,0 +1,15 @@
+package javaprogram2;
+
+public class Exe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The given specified expression is:");
+		System.out.println("(25.5*3.5-3.5*3.5)/(40.5-4.5)");
+		double c = (25.5*3.5-3.5*3.5)/(40.5-4.5);
+		System.out.println("After calculating the result is = " + c);
+		
+
+	}
+
+}
---
+++ Operator.java
@@ -0,0 +1,26 @@
+package javaprogram2;
+
+public class Operator {
+	public static void main(String[] args) {
+		int a = 10;
+		int b= 20;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		 
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<b&&b>a);
+		System.out.println(a<b||b>a);
+		System.out.println(a==b);
+		System.out.println(a++ + ++b);
+		System.out.println(b++ + ++a);
+		
+	}
+}
+		
+
+
