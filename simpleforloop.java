9f1281c5b2e8fef41b694123e86849f6100a655d
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-08-23 16:55:30 +0530

    Add files via upload

---
+++ simpleforloop.java
@@ -0,0 +1,15 @@
+package javaprogram2;
+
+public class simpleforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+		System.out.println();
+
+		}
+		}
+	}
